a9a000a8ab074b1a7c7f60a3fbcfec5f7ceed0ca
Raphael Luckom <RaphaelLuckom@gmail.com>
2015-03-19 21:15:39 -0400

    Timeline working.

---
+++ app/js/app.js
@@ -0,0 +1,29 @@
+var networkVisApp = angular.module('networkVisApp', [
+  "ngRoute",
+]);
+
+networkVisApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+  when('/about', {
+    templateUrl: 'partials/about.html',
+    controller: 'aboutController'
+  }).
+  when('/vis', {
+    templateUrl: 'partials/flowvis.html',
+    controller: 'chordController'
+  }).
+  otherwise({
+    redirectTo: '/vis'
+  });
+}]);
+
+networkVisApp.factory("ipUtils", function() {
+  return {
+    getPort: function(str) {
+      return /\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}:(\d{1,5})/.exec(str)[1]
+    },
+    getIP: function(str) {
+      return /(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})/.exec(str)[1]
+    }
+  }
+});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,110 +1,83 @@
-var networkVisApp = angular.module('networkVisApp', [
-  "ngRoute",
-]);
-
-networkVisApp.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-  when('/about', {
-    templateUrl: 'partials/about.html',
-    controller: 'aboutController'
-  }).
-  when('/vis', {
-    templateUrl: 'partials/flowvis.html',
-    controller: 'chordController'
-  }).
-  otherwise({
-    redirectTo: '/vis'
-  });
-}]);
-
-networkVisApp.factory("ipUtils", function() {
-  return {
-    getPort: function(str) {
-      return /\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}:(\d{1,5})/.exec(str)[1]
-    },
-    getIP: function(str) {
-      return /(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})/.exec(str)[1]
-    }
-  }
-});
-
-
-var makeHive = function(a, element) {
-  element.empty();
-  var degrees = function(rads) { return rads / Math.PI * 180 - 90;};
-
-  var svg = d3.select(element[0]).append('svg')
-  .append('g')
-  .attr('transform', "translate(" + a.center.x + "," + a.center.y + ")");
-
-  var mouseFunctionFactory = function(f) {
-    return function(d, thiz) {
-      if (typeof f !== 'function') {
-        return;
-      }
-      return f(d, 
-               svg.selectAll('.link'), 
-               svg.selectAll('.node circle'), 
-               svg.selectAll('.active'),
-               thiz);
-    };
-  };
-
-
-  svg.selectAll(".axis")
-  .data(a.axes)
-  .enter().append("line")
-  .attr("class", "axis")
-  .attr("transform", function(d) {return "rotate(" + degrees(d.angle) + ")"; })
-  .attr("x1", function (d) {return d.x1;})
-  .attr("x2", function (d) {return d.x2;})
-  .on("mouseover", function(d) {return mouseFunctionFactory(d.mouseover)(d, d3.select(this));})
-  .on("mouseout", function(d) {return mouseFunctionFactory(d.mouseout)(d, d3.select(this)); });
-
-  svg.append("g")
-  .attr("class", "links")
-  .selectAll(".link")
-  .data(a.links)
-  .enter().append("path")
-  .attr("class", "link")
-  .attr("d", d3.hive.link())
-  .on("mouseover", function(d) {return mouseFunctionFactory(d.mouseover)(d, d3.select(this));})
-  .on("mouseout", function(d) {return mouseFunctionFactory(d.mouseout)(d, d3.select(this)); });
-
-  window.circTypes = [];
-
-  svg.append("g")
-  .attr("class", "nodes")
-  .selectAll("circle")
-  .data(a.nodes)
-  .enter().append("circle")
-  .attr("class", "node")
-  .attr("transform", function(d) { return "rotate(" + degrees(d.angle) + ")";})
-    .attr("cx", function(d) {return d.radius;})
-    .attr("r", 4)
-    .on("mouseover", function(d) {return mouseFunctionFactory(d.mouseover)(d, d3.select(this));})
-    .on("mouseout", function(d) {return mouseFunctionFactory(d.mouseout)(d, d3.select(this)); });
-}
-
-networkVisApp.directive('rlHivePlot', [function() {
-  var link = function(scope, element, attrs) {
-    scope.$watch(attrs.svgArgs,
-                 function() { if (scope.svgArgs) {return makeHive(scope.svgArgs, element);}});
-  };
-  return {
-            link: link,
-            scope: {
-              svgArgs: "="
-            }
-          };
-}]);
-
 networkVisApp.controller('aboutController', ["$scope", function($scope) {
-  return $scope;}]); 
+  return $scope;}
+]); 
 
 networkVisApp.controller('chordController', ['$scope', '$http', 'ipUtils', 
-  function($scope, $http, ipUtils) {
-    var redrawHive = function(data) {
+  function($scope, $http, ipUtils) { 
+
+    /**
+    * clear highlighted svg elements on mouseout
+    *
+    * @param d [Object] Data associated with svg element by d3
+    * @param links [Array] array of .link elements selected by d3
+    * @param nodes [Array] array of '.node circle' elements selected by d3
+    * @param activeNodes [Array] array of '.active' nodes selected by d3
+    * @param thisElement [Object] d3 selection of current element.
+    */
+    var mouseout = function(d, links, nodes, activeNodes, thisElement) {
+      activeNodes.classed("active", false);
+      $scope.description = $scope.defaultInfo;
+      $scope.$apply();
+    };
+
+    var recordToString = function(r) {
+      return (
+        "NetFlow Record:\n  Time: " +  r.start + "\n  Duration: " 
+        + r.duration + "\n  Source: " + r.sourceWithPort + "\n  Dest: " 
+        + r.destWithPort + "\n  Protocol No: " + r.protocol + "\n  Bytes: " 
+        + r.bytes
+      );
+    }
+
+    /**
+    * Highlight connected paths on node mouseover
+    *
+    * @param d [Object] Data associated with svg element by d3
+    * @param links [Array] array of .link elements selected by d3
+    * @param nodes [Array] array of '.node circle' elements selected by d3
+    * @param activeNodes [Array] array of '.active' nodes selected by d3
+    * @param thisElement [Object] d3 selection of current element.
+    */
+    var nodeMouseOver = function(d, links, nodes, activeNodes, thisElement) {
+      links.classed("active", function(p) { return p.source === d || p.target === d; });
+      thisElement.classed("active", true);
+      if (d.type === "flow")  {
+        $scope.description = recordToString(d.record);
+      }
+      if (d.type === "source")  {
+        $scope.description = "Each path is to a flow that originated on port " + d.port;
+      }
+      if (d.type === "target")  {
+        $scope.description = "Each path is to a flow that ended on port " + d.port;
+      }
+      $scope.$apply();
+    };
+
+    /**
+    * highlight path on mouseover
+    *
+    * @param d [Object] Data associated with svg element by d3
+    * @param links [Array] array of .link elements selected by d3
+    * @param nodes [Array] array of '.node circle' elements selected by d3
+    * @param activeNodes [Array] array of '.active' nodes selected by d3
+    * @param thisElement [Object] d3 selection of current element.
+    */
+    var linkMouseOver = function(d, links, nodes, activeNodes, thisElement) {
+      var flow = d.source.type == "flow" ? d.source : d.target;
+      links.classed("active", function(p) { return p.source === flow || p.target === flow; });
+      $scope.description = recordToString(flow.record);
+      $scope.$apply();
+    };
+
+    /**
+     * sets up the graph of ports and flowrecords
+     *
+     * @param data [Object] JSON data of netflow records
+     */
+    var setupHive = function(data) {
+      $scope.defaultInfo = ("Showing source and destination ports for " 
+      + data.length + " netflow records");
+      $scope.description = $scope.defaultInfo; 
       var innerRadius = 100,
       outerRadius = 900,
       majorAngle = 2 * Math.PI / 3,
@@ -114,136 +87,125 @@ networkVisApp.controller('chordController', ['$scope', '$http', 'ipUtils',
 
       // D3 scale objects translate between input & output domains
       var angle = d3.scale.ordinal()
-      .domain(['source-port', 'flow', 'target-port'])
+      .domain(['source', 'flow', 'target'])
       .range([ -2 * minorAngle, 0, 2 * minorAngle]);
       var radius = d3.scale.linear()
-      .domain([0, data.length])
+      .domain([0, data.length + 1])
       .range([innerRadius, outerRadius]);
-
-
-      var defaultInfo = "Showing source and destination ports for " + data.length + " netflow records";
-      $scope.description = defaultInfo; 
 
       data.forEach(function(node) {
         node.sourcePort = ipUtils.getPort(node.sourceWithPort);
         node.destPort = ipUtils.getPort(node.destWithPort);
       });
-        
-      var mouseout = function(d, links, nodes, activeNodes, thisElement) {
-        activeNodes.classed("active", false);
-        $scope.description = defaultInfo;
-        $scope.$apply();
-      };
-      var nodeMouseOver = function(d, links, nodes, activeNodes, thisElement) {
-        links.classed("active", function(p) { return p.source === d || p.target === d; });
-        thisElement.classed("active", true);
-        if (d.type === "flow")  {
-          $scope.description = d.record;
-        }
-        if (d.type === "source-port")  {
-          $scope.description = "Source port " + d.port + " traffic. Each path is to a request that originated on that port.";
-        }
-        if (d.type === "target-port")  {
-          $scope.description = "Destination port " + d.port + " traffic. Each path is to a request that ended on that port.";
-        }
-        $scope.$apply();
 
-      };
-
-      var linkMouseOver = function(d, links, nodes, activeNodes, thisElement) {
-        links.classed("active", function(p) { return p === d; });
-        nodes.classed("active", function(p) { return p === d.source || p === d.target; });
-      }
       var axes = [
-                   {angle: angle('source-port'), x1: radius(-2), x2: radius(data.length)},
-                   {angle: angle('target-port'), x1: radius(-2), x2: radius(data.length)},
-                   {angle: angle('flow'), x1: radius(-2), x2: radius(data.length)}
-                 ],
-          nodeList = [],
-          connPaths = [],
-          sourceNodes = {}
-          destNodes = {};
+        {angle: angle('source'), x1: radius(-2), x2: radius(data.length + 1)},
+        {angle: angle('target'), x1: radius(-2), x2: radius(data.length + 1)},
+        {angle: angle('flow'), x1: radius(-2), x2: radius(data.length + 1)}
+      ],
+      nodeList = [],
+      connPaths = [],
+      sourceNodes = {}
+      destNodes = {};
+      $scope.svgArgs = {
+        center: center,
+        axes: axes,
+        links: connPaths,
+        nodes: nodeList
+      };
+      if (data.length == 0) { 
+        return; 
+      }
+      var tMax = tMin = moment(data[0].start), n, t;
+      for (n = 0; n < data.length; n++) {
+        t = moment(data[n].start);
+        if (t < tMin) { tMin = t; }
+        if (t > tMax) { tMax = t; }
+      }
+      var timeScale = d3.time.scale()
+      .domain([tMin, tMax])
+      .range([innerRadius, outerRadius]);
 
       var portScale = d3.scale.log()
-        .domain([1, 65536])
-        .range([innerRadius, outerRadius]);
+      .domain([1, 65536])
+      .range([innerRadius, outerRadius]);
 
       data.map(function(d, i, a) {
         var sourcePortNode = sourceNodes[d.sourcePort] ? sourceNodes[d.sourcePort] :
-                             sourceNodes[d.sourcePort] = {
-                                angle: angle('source-port'),
-                                port: d.sourcePort,
-                                type: 'source-port',
-                                radius: portScale(parseInt(d.sourcePort) + 1),
-                                mouseover: nodeMouseOver,
-                                mouseout: mouseout
-                              },
-            destPortNode = destNodes[d.destPort] ? destNodes[d.destPort] :
-                           destNodes[d.destPort] = {
-                              angle: angle('target-port'),
-                              type: 'target-port',
-                              port: d.destPort,
-                              radius: portScale(parseInt(d.destPort) + 1),
-                              mouseover: nodeMouseOver,
-                              mouseout: mouseout
-                            },
-            flowNode = {
-                          angle: angle('flow'),
-                          type: 'flow',
-                          record: d,
-                          radius: radius(i),
-                          mouseover: nodeMouseOver,
-                          mouseout: mouseout
-                       };
+        sourceNodes[d.sourcePort] = {
+          angle: angle('source'),
+          port: d.sourcePort,
+          type: 'source',
+          radius: portScale(parseInt(d.sourcePort) + 1),
+          mouseover: nodeMouseOver,
+          mouseout: mouseout
+        },
+        destPortNode = destNodes[d.destPort] ? destNodes[d.destPort] :
+        destNodes[d.destPort] = {
+          angle: angle('target'),
+          type: 'target',
+          port: d.destPort,
+          radius: portScale(parseInt(d.destPort) + 1),
+          mouseover: nodeMouseOver,
+          mouseout: mouseout
+        },
+        flowNode = {
+          angle: angle('flow'),
+          type: 'flow',
+          record: d,
+          radius: timeScale(moment(d.start)),
+          mouseover: nodeMouseOver,
+          mouseout: mouseout
+        };
         nodeList.push(sourcePortNode);
         nodeList.push(destPortNode);
         nodeList.push(flowNode);
         connPaths.push({
-                         source: sourcePortNode, 
-                         target: flowNode, 
-                         mouseover: linkMouseOver,
-                         mouseout: mouseout
-                       });
+          source: sourcePortNode, 
+          target: flowNode, 
+          mouseover: linkMouseOver,
+          mouseout: mouseout
+        });
         connPaths.push({
-                         source: flowNode, 
-                         target: destPortNode,
-                         mouseover: linkMouseOver,
-                         mouseout: mouseout
-                       });
+          source: flowNode, 
+          target: destPortNode,
+          mouseover: linkMouseOver,
+          mouseout: mouseout
+        });
       });
 
       $scope.svgArgs = {
-                  center: center,
-                  axes: axes,
-                  links: connPaths,
-                  nodes: nodeList
-                };
-                 
-
+        center: center,
+        axes: axes,
+        links: connPaths,
+        nodes: nodeList
+      };
     };
 
-
-    $http.get('../netflows.json').success(function(data) {
+    $http.get('../netflow5.json').success(function(data) {
       $scope.hiveData = data;
       $scope.filteredData = data;
       $scope.$watch(function() {return $scope.filteredData;},
-                    function() {return redrawHive($scope.filteredData);});
+      function() {return setupHive($scope.filteredData);});
       var filterHiveData = function() {
         var hd = $scope.hiveData;
         if ($scope.sourceIp) {
-           hd = hd.filter(function(d) {
-             return d.source.indexOf($scope.sourceIp) !== -1;
-           });
-         }
+          var re = new RegExp($scope.sourceIp);
+          hd = hd.filter(function(d) {
+            return re.test(d.sourceWithPort);
+          });
+        }
         if ($scope.destIp) {
-           hd = hd.filter(function(d) {
-             return d.dest.indexOf($scope.destIp) !== -1;
-           });
-         }
-         $scope.filteredData = hd;
-       };
+          var re = new RegExp($scope.destIp);
+          hd = hd.filter(function(d) {
+            return re.test(d.destWithPort);
+          });
+        }
+        $scope.filteredData = hd;
+      };
 
       $scope.$watch(function() {return $scope.sourceIp;}, filterHiveData);
       $scope.$watch(function() {return $scope.destIp;}, filterHiveData);
     });
-  }]);
+  }
+]);
